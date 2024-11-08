f5106a1c7184939ff9386052e93dca72f8b7bece
Mika Manaligod <mmanaligod3@se12l0325-04.ad.bcit.ca>
2024-11-08 13:53:00 -0800

    add landing page components

---
+++ CreaturesOfHabit/Views/AboutView.swift
@@ -0,0 +1,60 @@
+//
+//  AboutView.swift
+//  CreaturesOfHabit
+//
+//  Created by Mika Manaligod on 2024-11-08.
+//
+
+
+import SwiftUI
+
+let teamMembers = ["Alfrey", "Ben", "Cheryl", "Conrad", "Mika"]
+
+struct AboutView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack(spacing: 20) {
+                
+                Text("About the App")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.top)
+                
+                
+                Text("Creatures of Comfort helps you build healthy habits by caring for a pet. Build your habits and help your pet thrive!")
+                    .font(.body)
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.center)
+                
+                
+                
+                
+                NavigationLink(destination: HatchPetView()) {
+                    Text("Start your journey!")
+                        .padding()
+                        .background(Color.purple)
+                        .foregroundColor(.white)
+                        .cornerRadius(30)
+                        .fontWeight(.bold)
+                    
+                }.padding(.top, 20)
+                
+            }
+            Spacer()
+                NavigationLink(destination: CreditsView()) {
+                    Text("Credits")
+                        .foregroundColor(.black)
+                        .fontWeight(.semibold)
+                    
+                } .padding(.bottom, 30)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+    }
+
+
+#Preview {
+    AboutView()
+}
---
+++ CreaturesOfHabit/Views/CreditsView.swift
@@ -0,0 +1,37 @@
+//
+//  CreditsView.swift
+//  CreaturesOfHabit
+//
+//  Created by Mika Manaligod on 2024-11-08.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    let teamMembers = ["Alfrey", "Ben", "Cheryl", "Conrad", "Mika"]
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            Text("Team Members")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top, 30)
+            
+            VStack(spacing: 10) {
+                ForEach(teamMembers, id: \.self) {
+                    member in Text(member)
+                        .font(.body)
+                }
+            }.padding(.horizontal)
+            Spacer()
+        }
+        .frame(maxHeight: .infinity, alignment: .center)
+        
+    }
+}
+
+
+#Preview {
+    CreditsView()
+}
