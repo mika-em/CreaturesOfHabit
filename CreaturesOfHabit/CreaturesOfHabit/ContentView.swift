fad4013a158a6688f1808ceaf6741da24d7c2727
Mika Manaligod <mmanaligod3@se12l0325-04.ad.bcit.ca>
2024-11-08 13:46:30 -0800

    initial commit

---
+++ CreaturesOfHabit/CreaturesOfHabit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CreaturesOfHabit
+//
+//  Created by Mika Manaligod on 2024-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CreaturesOfHabit/CreaturesOfHabit/CreaturesOfHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreaturesOfHabitApp.swift
+//  CreaturesOfHabit
+//
+//  Created by Mika Manaligod on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct CreaturesOfHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
