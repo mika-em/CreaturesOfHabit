8b010d108082ca68bb5b2eac48825914ff6b4e0c
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2024-12-02 23:22:16 -0800

    Add Creature leveling system and GoalLog progress logic

--- CreaturesOfHabit/Models/GoalLog.swift
+++ CreaturesOfHabit/Models/GoalLog.swift
@@ -17,12 +17,25 @@ class GoalLog {
     @Relationship var user: User
     @Relationship var goal: Goal
     
-    init(id: UUID, unitsTotal: Double, unitsCompleted: Double, isComplete: Bool, user: User, goal: Goal) {
+    init(id: UUID, unitsTotal: Double, unitsCompleted: Double = 0.0, isComplete: Bool = false, user: User, goal: Goal) {
         self.id = id
         self.unitsTotal = unitsTotal
         self.unitsCompleted = unitsCompleted
         self.isComplete = isComplete
         self.user = user
         self.goal = goal
+    }
+    
+    func incrementUnitsCompleted() {
+        self.unitsCompleted += 1
+        checkCompletion()
+    }
+        
+    private func checkCompletion() {
+        if self.unitsCompleted >= self.unitsTotal {
+            self.unitsCompleted = self.unitsTotal  // Prevent exceeding total?
+            self.isComplete = true
+            print("Goal completed!")
+        }
     }
 }
