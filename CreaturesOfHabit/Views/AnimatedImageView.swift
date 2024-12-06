bbbee1a3fd41d4a1fe00cdaf39d1eb22e414239d
Cheryl Lau <113461643+cherylau@users.noreply.github.com>
2024-12-06 01:38:01 -0800

    Fix animate image jitter

--- CreaturesOfHabit/Views/AnimatedImageView.swift
+++ CreaturesOfHabit/Views/AnimatedImageView.swift
@@ -9,37 +9,48 @@ import SwiftUI
 
 struct AnimatedImageView: View {
     @State private var showFirstImage = true
+    @State private var timer: Timer?  // Store the timer to control its lifecycle
+    
     let firstImageName: String
     let secondImageName: String
     let animationDuration: Double
     
     var body: some View {
         VStack {
-            // Display the current image
             Image(showFirstImage ? firstImageName : secondImageName)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 250, height: 250)
                 .transition(.opacity) // Use opacity for a smooth transition
-            .onAppear {
-                startImageCycle()
-            }
+                .onAppear {
+                    startImageCycle()
+                }
+                .onDisappear {
+                    stopImageCycle()
+                }
         }
     }
     
-    // Method to cycle the images at regular intervals
     private func startImageCycle() {
-        Timer.scheduledTimer(withTimeInterval: animationDuration, repeats: true) { _ in
-            showFirstImage.toggle()  // No animation
+        guard timer == nil else { return }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: animationDuration, repeats: true) { _ in
+            showFirstImage.toggle()
         }
     }
+    
+    func stopImageCycle() {
+        timer?.invalidate()
+        timer = nil
+    }
 }
+
 
 
 #Preview {
     AnimatedImageView(
         firstImageName: "dragon_baby",
         secondImageName: "dragon_baby2",
-        animationDuration: 0.3
+        animationDuration: 0.4
     )
 }
