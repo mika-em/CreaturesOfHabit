ca444112a7e874972ad356e82878544fab7cf2f5
Mika <mkmanaligod@gmail.com>
2024-12-06 01:03:33 -0800

    add theme files

---
+++ Theme/ThemedButtonStyle.swift
@@ -0,0 +1,29 @@
+//
+//  ThemedButtonStyle.swift
+//  CreaturesOfHabit
+//
+//  Created by Mika M on 2024-12-05.
+//
+
+import SwiftUI
+
+struct ThemedButtonStyle: ButtonStyle {
+    @Environment(\.theme) private var theme
+    var backgroundColor: Color
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(Font(theme.fonts.buttonFont))
+            .frame(height: theme.buttons.buttonHeight)
+            .padding(.horizontal, 20)
+            .background(
+                configuration.isPressed
+                    ? backgroundColor.opacity(0.8)
+                    : backgroundColor
+            )
+            .foregroundColor(Color(theme.colors.primaryButtonForeground)) // Text color
+            .cornerRadius(theme.buttons.cornerRadius) // Rounded corners
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0) // Press animation
+            .animation(.easeInOut(duration: 0.2), value: configuration.isPressed) // Smooth animation
+    }
+}
