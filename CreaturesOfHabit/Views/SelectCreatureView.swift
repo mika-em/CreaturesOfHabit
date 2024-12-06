faaaa153faa278eb8fb50976d63a3a1b6ebef359
Mika <mkmanaligod@gmail.com>
2024-12-06 02:11:04 -0800

    add theme to select creature view

--- CreaturesOfHabit/Views/SelectCreatureView.swift
+++ CreaturesOfHabit/Views/SelectCreatureView.swift
@@ -13,62 +13,77 @@ struct SelectCreatureView: View {
     @State private var selectedCreature: PredefinedCreature? = nil
     @ObservedObject var userViewModel: UserViewModel
     @State private var showHatchPetView = false
+    @Environment(\.theme) private var theme
 
     var body: some View {
-        VStack {
-            Text("Select your creature")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding()
-
-            // List of predefined creatures
-            List(predefinedCreatures, id: \.type) { creature in
-                HStack {
-                    Image("\(creature.type.lowercased())_\(creature.state.lowercased())")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100, height: 100)
-                        .clipShape(Circle())
-
-                    Text(creature.type.capitalized)
-                        .font(.title2)
-                        .fontWeight(.medium)
-
-                    Spacer()
-
-                    if selectedCreature?.type == creature.type {
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundColor(.green)
+        ZStack {
+            LinearGradient(
+                gradient: theme.gradients.defaultGradient,
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            VStack(spacing:15) {
+                Spacer()
+                Spacer()
+                Text("Select your creature")
+                    .font(Font(theme.fonts.headerFont))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(theme.colors.primaryText))
+                // List of predefined creatures
+                List(predefinedCreatures, id: \.type) { creature in
+                    HStack {
+                        Image("\(creature.type.lowercased())_\(creature.state.lowercased())")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                        
+                        Text(creature.type.capitalized)
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        
+                        Spacer()
+                        
+                        if selectedCreature?.type == creature.type {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(.purple)
+                        }
+                    }
+                    .padding()
+                    .background(selectedCreature?.type == creature.type ? Color.purple.opacity(0.2) : Color.clear)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        selectCreature(creature: creature)
                     }
                 }
+                .scrollContentBackground(.hidden)
                 .padding()
-                .background(selectedCreature?.type == creature.type ? Color.purple.opacity(0.2) : Color.clear)
-                .cornerRadius(10)
-                .onTapGesture {
-                    selectCreature(creature: creature)
+                .background(Color.clear)
+                
+                if let selectedCreature = selectedCreature {
+                    Button("Confirm Selection") {
+                        saveSelectedCreature(creature: selectedCreature)
+                        showHatchPetView = true
+                    }
+                    .padding()
+                    .background(Color(theme.colors.primaryButtonBackground))
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+                    .sheet(isPresented: $showHatchPetView) {
+                        HatchPetView(onHatchComplete: {
+                            dismiss() // Dismiss SelectCreatureView after hatching
+                        }).environmentObject(userViewModel)
+                    }
                 }
+                
+                Spacer()
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             .padding()
-
-            if let selectedCreature = selectedCreature {
-                Button("Confirm Selection") {
-                    saveSelectedCreature(creature: selectedCreature)
-                    showHatchPetView = true
-                }
-                .padding()
-                .background(Color.green)
-                .foregroundColor(.white)
-                .cornerRadius(20)
-                .padding()
-                .sheet(isPresented: $showHatchPetView) {
-                    HatchPetView(onHatchComplete: {
-                        dismiss() // Dismiss SelectCreatureView after hatching
-                    }).environmentObject(userViewModel)
-                }
-            }
-        }
-    }
-
+        }}
+    
+    
     private func selectCreature(creature: PredefinedCreature) {
         selectedCreature = creature
     }
